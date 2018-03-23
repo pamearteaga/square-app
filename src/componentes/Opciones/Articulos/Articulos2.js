e7b4c44590f68d2b683f09d61329f5b604ddf260
pamearteaga <pe_arteaga@hotmail.com>
2018-03-23 00:00:42 -0300

    sección articulos

--- src/componentes/Opciones/Articulos/Articulos2.js
+++ src/componentes/Opciones/Articulos/Articulos2.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import Modal from './Modal.js';
 import data from './mock.json';
+import Stock from './../../Stock/Stock';
+import Venta from './../../Venta/Venta';
+import Product from './../../Product/Product';
+import CreateProduct from './../../CreateProduct/CreateProduct';
+import firebase from 'firebase';
 import './Articulos.css';
 
 
@@ -42,6 +47,39 @@ class Articulos extends Component{
   render() {
     return(
       <div id="btnAr" className="Articulos">
+
+        <div className="products-container">
+        <header>
+          <h1>Catálogo</h1>
+          <Stock stock={this.props.stock} />
+          <Venta ventas={this.props.img} />
+        </header>
+
+        <div className="products-list">
+          <ul>
+            {
+              this.props.products.map(product => {
+                console.log(product.id)
+                return (
+                  <Product
+                    name={product.name}
+                    key={product.id}
+                    stock={product.stock}
+                    img={product.img}
+                    id={product.id}
+                    removeProduct={this.props.removeProduct}
+                  />
+                )
+              })
+            }
+             <CreateProduct
+              addProduct={this.props.addProduct}
+            />
+           
+          </ul>
+        </div>
+      </div>
+      <Modal/>
         <div className='search-container'>
           <input type='text' id='search' placeholder='Busca entre tus productos' onKeyUp={this.filterProducts} ref={(input) => { this.inputFilter = input; }}/>
         </div>
--- src/componentes/Opciones/Teclado/Teclado.js
+++ src/componentes/Opciones/Teclado/Teclado.js
@@ -5,12 +5,155 @@ import './Teclado.css';
 class Teclado extends Component{
 
   tecla1 = (event) => {
-    const btn = document.getElementById('totalCompra').innerHTML;
-    const operacion= document.getElementById('operacion');
-    operacion.innerHTML = + "1";
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "1";
+         btn.innerHTML = "1";
+    } else {
+       operacion.innerHTML = operacionInner + "1";
+       btn.innerHTML = btnInner + "1";
+    }
+  }
+  tecla2 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "2";
+         btn.innerHTML = "2";
+    } else {
+       operacion.innerHTML = operacionInner + "2";
+       btn.innerHTML = btnInner + "2";
+    }
+  }
+  tecla3 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "3";
+         btn.innerHTML = "3";
+    } else {
+       operacion.innerHTML = operacionInner + "3";
+       btn.innerHTML = btnInner + "3";
+    }
+  }
+  tecla4 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "4";
+         btn.innerHTML = "4";
+    } else {
+       operacion.innerHTML = operacionInner + "4";
+       btn.innerHTML = btnInner + "4";
+    }
+  }
+  tecla5 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "5";
+         btn.innerHTML = "5";
+    } else {
+       operacion.innerHTML = operacionInner + "5";
+       btn.innerHTML = btnInner + "5";
+    }
+  }
+  tecla6 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "6";
+         btn.innerHTML = "6";
+    } else {
+       operacion.innerHTML = operacionInner + "6";
+       btn.innerHTML = btnInner + "6";
+    }
+  }
+  tecla7 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "7";
+         btn.innerHTML = "7";
+    } else {
+       operacion.innerHTML = operacionInner + "7";
+       btn.innerHTML = btnInner + "7";
+    }
+  }
+  tecla8 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "8";
+         btn.innerHTML = "8";
+    } else {
+       operacion.innerHTML = operacionInner + "8";
+       btn.innerHTML = btnInner + "8";
+    }
+  }
+  tecla9 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "9";
+         btn.innerHTML = "9";
+    } else {
+       operacion.innerHTML = operacionInner + "9";
+       btn.innerHTML = btnInner + "9";
+    }
+  }
+  tecla0 = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const btnInner = document.getElementById('totalCompra').innerHTML;
+    const operacion = document.getElementById('operacion');
+    const operacionInner = document.getElementById('operacion').innerHTML;
+
+    if (operacionInner === "0" ) {
+         operacion.innerHTML = "0";
+         btn.innerHTML = "0";
+    } else {
+       operacion.innerHTML = operacionInner + "0";
+       btn.innerHTML = btnInner + "0";
+    }
+  }
+  teclaBorrar = (event) => {
+    const btn = document.getElementById('totalCompra');
+    const operacion = document.getElementById('operacion');
+    operacion.innerHTML = "0";
+    btn.innerHTML = "0";
   }
 
-  
+
+
+
   render() {
     return(
       <div id="btnTe" className="Teclado hidden">
@@ -20,7 +163,7 @@ class Teclado extends Component{
         <div className="col-xs-4" onClick={this.tecla1}>
           <h1 id="1">1</h1>
         </div>
-        <div className="col-xs-4 tecla">
+        <div className="col-xs-4" onClick={this.tecla2}>
           <h1 id="2">2</h1>
         </div>
         <div className="col-xs-4 tecla">
